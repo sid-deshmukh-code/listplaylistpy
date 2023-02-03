51400768676a6ae85adeca5357dd2670246ea4fa
Sid <deshmukhsiddhant420@gmail.com>
2023-02-04 00:45:10 +0530

    if it works dont touch it

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request  
+from pytube import Playlist
 
 
 
@@ -8,15 +9,23 @@ app = Flask(__name__)
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == 'POST':
-        url = request.form.get('url')
-        flag = "="
+        # getting url from HTML page
+        url = request.form.get('url')      
+
+        # getting playlist URLs
+        p = Playlist(url)
+                      
+        url_list = []                  # empty list
+
+        for u_id in p.video_urls:
+            
+            print(u_id)
+            index = u_id.index("=")
+            vid_id = u_id[index+1:]   #video is stored in vid_id
+            url_list.append(vid_id)
         
-        index = url.index(flag)
-        a = url[index+1:]
-        print("Character found at index", index)
-        print(a)
-    
-        return render_template('result.html', name=a)
+        # print(url_list)
+        return render_template('result.html', name=url_list)   # 
     return render_template('index.html')
 
 
